00e3238701d79661c449c5b398fca407bb9c0533
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2023-09-21 20:10:44 -0300

    Arrumando último exercício

--- Terceira lista/ex14/PrePosIncrementoDecremento.java
+++ Terceira lista/ex14/PrePosIncrementoDecremento.java
@@ -17,9 +17,9 @@ package ex14;
 public class PrePosIncrementoDecremento {
     public static void main(String[] args) {
         int a = 1;
-        System.out.println(a);
-        System.out.println(a += 2);
-        System.out.println(a);
-        System.out.println(a -= 2);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
     }
 }
